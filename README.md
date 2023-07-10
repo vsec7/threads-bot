655a5a12810036c8ebd1fc1039715f8e7228893f
Viloid <vsec48@gmail.com>
2023-07-10 18:51:13 +0700

    v1

---
+++ README.md
@@ -0,0 +1,33 @@
+# Auto Post Random Quote Threads.net
+
+Auto Posts random quote threads.net
+
+Crafted by viloid (github.com/vsec7)
+
+## Installation
+
+```bash
+git clone github.com/vsec7/threads-bot.git
+cd threads-bot
+npm i
+
+```
+
+## Credentials
+
+Rename .env.example to .env
+```
+UNAME="YOUR_INSTAGRAM_USERNAME"
+PASSW="YOUR_INSTAGRAM_PASSWORD"
+DELAY=120 # delay in seconds . 120 sec = 2 mins.
+```
+
+## Run
+
+```bash
+npm start
+```
+
+## Reference API Wrapper
+
+https://github.com/junhoyeo/threads-api
---
+++ index.js
@@ -0,0 +1,33 @@
+const { ThreadsAPI } = require('threads-api');
+const axios = require('axios');
+require('dotenv').config();
+
+async function randomQuote() {
+    try {
+      const response = await axios.get('https://gist.githubusercontent.com/vsec7/45b5b362d9dde8bd381dc9b8a3b6331a/raw/7934009fc02655de3ffc4a28af22a6c0c1ecd102/quote-id.json');
+      const quotes = response.data;
+      const randomIndex = Math.floor(Math.random() * quotes.length);
+      const randomQuote = quotes[randomIndex]['quote'];
+      return randomQuote;
+    } catch (error) {
+      throw new Error('Failed to retrieve random quote.');
+    }
+}
+
+const main = async () => {
+    const threadsAPI = new ThreadsAPI({
+        username: process.env.UNAME,
+        password: process.env.PASSW
+    });
+
+    const q = await randomQuote();
+    const p = await threadsAPI.publish({
+      text: q,
+    });
+
+    console.log(p)
+}
+  
+setInterval( async () => {
+        await main()
+    }, process.env.DELAY * 1000);
